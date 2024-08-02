075069d68ebe516229f954d295d23c3a59c154b8
akhil-haridas <4khilharidas@gmail.com>
2024-08-02 17:52:02 +0530

    removed file change action logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { PotreeViewer, SideToolBar } from "./components";
 
-const POINT_CLOUDS = [
-  "http://5.9.65.151/mschuetz/potree/resources/pointclouds/helimap/360/MLS_drive1/cloud.js",
-  "http://5.9.65.151/mschuetz/potree/resources/pointclouds/helimap/epalinges/als_converted/cloud.js",
-  "http://5.9.65.151/mschuetz/potree/resources/pointclouds/archpro/heidentor/cloud.js",
-];
-
 function App() {
-  const [cloudUrl, setCloudUrl] = useState(POINT_CLOUDS[0]);
-
-  const onChangePointCloud = (target) => {
-    const currentIndex = POINT_CLOUDS.findIndex((url) => url === cloudUrl);
-    if (currentIndex === 0 && target === -1) {
-      setCloudUrl(POINT_CLOUDS[POINT_CLOUDS.length]);
-    } else if (currentIndex === POINT_CLOUDS.length && target === 1) {
-      setCloudUrl(POINT_CLOUDS[0]);
-    }
-    setCloudUrl(POINT_CLOUDS[currentIndex + target]);
-  };
   return (
     <div
       className="potree_container"
@@ -31,12 +14,8 @@ function App() {
         top: 0,
       }}
     >
-      <PotreeViewer cloudUrl={cloudUrl} />
+      <PotreeViewer cloudUrl={"http://5.9.65.151/mschuetz/potree/resources/pointclouds/helimap/360/MLS_drive1/cloud.js"} />
       <SideToolBar />
-      <div className="potree_controls">
-        <button onClick={() => onChangePointCloud(-1)}>Back</button>
-        <button onClick={() => onChangePointCloud(1)}>Next</button>
-      </div>
     </div>
   );
 }
